28a79a4e2d16476f6616cafd470591c5334efaab
NoOneWillSeeIt <rest338@gmail.com>
2024-03-14 12:31:53 +0700

    redis helper

--- webapp/core/__init__.py
+++ webapp/core/__init__.py
@@ -1,4 +1,6 @@
-from .settings import settings
 from .db_helper import DatabaseHelper
+from .redis_helper import RedisHelper
+from .settings import settings
 
 db_helper = DatabaseHelper(settings.db.url, settings.db.echo)
+redis_helper = RedisHelper(settings.redis.host, settings.redis.port, settings.redis.db)
---
+++ webapp/core/redis_helper.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager, contextmanager
+from typing import AsyncGenerator, Generator
+from redis import Redis
+from redis.asyncio import Redis as AsyncRedis
+
+
+class RedisHelper:
+
+    def __init__(self, host: str, port: int, db: int):
+        self.redis = Redis(host, port, db)
+        self.async_redis = AsyncRedis(host=host, port=port, db=db)
+
+    @asynccontextmanager
+    async def async_connection(self) -> AsyncGenerator[AsyncRedis, None]:
+        conn = AsyncRedis(connection_pool=self.redis.connection_pool)
+        try:
+            yield conn
+        finally:
+            await conn.aclose()
+
+    @contextmanager
+    def connection(self) -> Generator[Redis, None, None]:
+        conn = Redis(connection_pool=self.redis)
+        try:
+            yield conn
+        finally:
+            conn.close()
+
+    async def async_redis_dependency(self) -> AsyncGenerator[AsyncRedis, None]:
+        async with self.async_connection() as conn:
+            yield conn
