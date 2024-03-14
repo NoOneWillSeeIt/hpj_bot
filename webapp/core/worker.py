a49289a408b90b31b2a3f34dcbfb65e3978ac58a
NoOneWillSeeIt <rest338@gmail.com>
2024-03-14 12:33:06 +0700

    report worker fixes

--- webapp/core/worker.py
+++ webapp/core/worker.py
@@ -2,27 +2,21 @@ import json
 import logging
 import multiprocessing
 import signal
-from collections import namedtuple
 from concurrent.futures import ProcessPoolExecutor
 
 import requests
-from redis import Redis
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from survey.hpj_questions import Questions
-from webapp.core.constants import Channel, ReportTaskProducer
+from webapp.core import redis_helper
+from webapp.core.constants import ReportTaskProducer
 from webapp.core.db_helper import DatabaseHelper
 from webapp.core.models import JournalEntry
 from webapp.journal_view.html_generator import HTMLGenerator
+from webapp.workers.redis_constants import RedisKeys, ReportTaskInfo
 
 from .settings import DbSettings, JinjaSettings, init_test_settings, settings
-
-TaskInfo = namedtuple(
-    "TaskInfo",
-    ["user_id", "channel", "channel_id", "producer", "start", "end"],
-    defaults=[None] * 6,
-)
 
 
 class GracefulKiller:
@@ -55,7 +49,7 @@ def init_process_worker(db_settings: DbSettings, jinja_settings: JinjaSettings):
     _process_logger = multiprocessing.get_logger()
 
 
-def read_entries_from_db(session: Session, info: TaskInfo) -> list[JournalEntry]:
+def read_entries_from_db(session: Session, info: ReportTaskInfo) -> list[JournalEntry]:
     stmt = (
         select(JournalEntry)
         .where(JournalEntry.user_id == info.user_id)
@@ -64,24 +58,9 @@ def read_entries_from_db(session: Session, info: TaskInfo) -> list[JournalEntry]
     return session.scalars(stmt).all()
 
 
-def parse_task_info(task_info: str) -> TaskInfo:
-    try:
-        splitted = task_info.split(";")
-        splitted[0] = int(splitted[0])  # user_id
-        splitted[1] = Channel(splitted[1])  # channel
-        splitted[2] = int(splitted[2])  # channel_id
-        splitted[3] = ReportTaskProducer(splitted[3])  # producer
-
-        return TaskInfo(*splitted)
-    except Exception:
-        logging.error(f'Can\'t parse task info: "{task_info}"')
-
-    return TaskInfo()
-
-
-def worker(info: TaskInfo, url_to_send: str):
+def worker(info: ReportTaskInfo, url_to_send: str):
     entry_rows = []
-    with _process_db_helper.session_context() as session:
+    with _process_db_helper.session() as session:
         entry_rows = read_entries_from_db(session, info)
 
     if not entry_rows:
@@ -107,19 +86,25 @@ def pool_handler(worker_count: int = 4, test_config: bool = False):
     if test_config:
         init_test_settings()
 
-    redis = Redis(host=settings.redis.url, port=settings.redis.port)
-    with ProcessPoolExecutor(
-        worker_count,
-        initializer=init_process_worker,
-        initargs=(settings.db, settings.jinja),
-    ) as pool:
+    with (
+        ProcessPoolExecutor(
+            worker_count,
+            initializer=init_process_worker,
+            initargs=(settings.db, settings.jinja),
+        ) as pool,
+        redis_helper.connection() as redis
+    ):
         gk = GracefulKiller()
         while not gk.exit_now:
-            task_key = redis.blpop(["gen_report_queue"], timeout=1)
+            task_key = redis.blpop([RedisKeys.reports_queue], timeout=1)
             if not task_key:
                 continue
 
-            info = parse_task_info(task_key)
+            info = ReportTaskInfo.from_str(task_key)
+            if not info:
+                logging.error(f'Can\'t parse task info: "{info}"')
+                continue
+
             channel_url = redis.get(f"{info.channel}_url")
             pool.submit(worker, info, f"{channel_url}/{info.channel_id}")
 
