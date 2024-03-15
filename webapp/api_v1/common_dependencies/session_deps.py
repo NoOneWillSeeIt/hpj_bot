6028e2c7ff16631eb469a6c8325e61d8ea9c3374
NoOneWillSeeIt <rest338@gmail.com>
2024-03-15 15:24:10 +0700

    redis dependency

--- webapp/api_v1/common_dependencies/session_deps.py
+++ webapp/api_v1/common_dependencies/session_deps.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
 from fastapi import Depends
+from redis.asyncio import Redis as AsyncRedis
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from webapp.core import db_helper
+from webapp.core import db_helper, redis_helper
 
 SessionDep = Annotated[AsyncSession, Depends(db_helper.async_session_dependency)]
+RedisDep = Annotated[AsyncRedis, Depends(redis_helper.async_redis_dependency)]
