ba76479761401ba965b7c400073f0bd2dcb6be92
NoOneWillSeeIt <rest338@gmail.com>
2024-03-15 14:56:54 +0700

    graceful killer adjust to raise exception

--- webapp/workers/utils.py
+++ webapp/workers/utils.py
@@ -1,15 +1,22 @@
 import signal
 
 
+class GracefulExit(SystemExit):
+    pass
+
+
 class GracefulKiller:
 
     exit_now = False
     signum = None
 
-    def __init__(self):
+    def __init__(self, raise_ex: bool = False):
+        self._raise_ex = raise_ex
         for sig_code in [signal.SIGINT, signal.SIGTERM]:
             signal.signal(sig_code, self.handle_signal)
 
     def handle_signal(self, signum, frame):
         self.exit_now = True
         self.signum = signum
+        if self._raise_ex:
+            raise GracefulExit(f'{signum}')
