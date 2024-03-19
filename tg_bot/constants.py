0add90dd79f2c15ae3c16e466efecf71828e400b
NoOneWillSeeIt <rest338@gmail.com>
2024-03-19 18:25:07 +0700

    bot_settings

--- tg_bot/constants.py
+++ tg_bot/constants.py
@@ -1,43 +1,39 @@
+import os
 from datetime import timedelta, timezone
 from enum import StrEnum, auto
-from os import getenv
 from pathlib import Path
 
+from pydantic import AliasChoices, Field
+from pydantic_settings import BaseSettings
 
-_CURRENT_FILE_PATH = Path(__file__).parent.resolve()
+
+BASE_DIR = Path(os.getcwd())
+BOT_PATH = BASE_DIR / "tg_bot"
 
 
 # PATHS
-DB_FOLDER = f"{_CURRENT_FILE_PATH}/db_instance"
-DB_PATH = f"{DB_FOLDER}/hpj_bot.db"
-TEST_DB_PATH = f"{DB_FOLDER}/test_hpj_bot.db"
-PERSISTENCE_PATH = f"{DB_FOLDER}/persistence"
-
-JINJA_TEMPLATE_PATH = f"{_CURRENT_FILE_PATH}/journal_view/templates"
-JOURNAL_TEMPLATE = "journal.html"
-
-STATIC_PATH = f"{_CURRENT_FILE_PATH}/static"
-FLASK_PIC_PATH = f"{STATIC_PATH}/flask.png"
-HEALTH_PIC_PATH = f"{STATIC_PATH}/health.png"
+PERSISTENCE_PATH = BASE_DIR / "persistence"
+STATIC_PATH = BASE_DIR / "static"
+FLASK_PIC_PATH = STATIC_PATH / "flask.png"
+HEALTH_PIC_PATH = STATIC_PATH / "health.png"
 
 # TIME
 MSK_TIMEZONE_OFFSET = timezone(timedelta(hours=3))
 ENTRY_KEY_FORMAT = "%d.%m"
 TIME_FORMAT = "%H:%M%z"
 
-# JOBS
-ALARM_JOB_PREFIX = "alarm"
-DAYS_TO_STORE = 60
-DAYS_TO_STORE_BACKUP = 30
 
-# TOKENS/IDS
-DEVELOPER_CHAT_ID = getenv("DEVELOPER_CHAT_ID", "")
-HPJ_BOT_TOKEN = getenv("HPJ_TOKEN", "")
-TEST_TOKEN = getenv("HPJ_TEST_TOKEN", "")
-TEST_CHAT_ID = getenv("HPJ_TEST_CHAT", "")
+class BotSettings(BaseSettings):
+    developer_chat_id: str = Field(default='', validation_alias=AliasChoices('DEVELOPER_CHAT_ID'))
+    token: str = Field(default='', validation_alias=AliasChoices('HPJ_TOKEN'))
+    test_token: str = Field(default='', validation_alias=AliasChoices('HPJ_TEST_TOKEN'))
+    test_chat: str = Field(default='', validation_alias=AliasChoices('HPJ_TEST_CHAT'))
 
 
 class OutputFileFormats(StrEnum):
     HTML = auto()
     # currently under construction
     # PDF = auto()
+
+
+bot_settings = BotSettings()
