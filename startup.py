4f21a3816c39f98215d9d9e9150bd16cb01c1d10
NoOneWillSeeIt <rest338@gmail.com>
2024-03-19 18:30:09 +0700

    cli instruments to start services

--- startup.py
+++ startup.py
@@ -1,16 +1,6 @@
-from datetime import datetime, timedelta, time
 import logging
-import os
-import sqlite3
 
-from telegram import Update
-
-from tg_bot.bot import configure_app
-from tg_bot.constants import ALARM_JOB_PREFIX, DB_FOLDER, DB_PATH, MSK_TIMEZONE_OFFSET, TIME_FORMAT
-import tg_bot.db.queries as db
-from tg_bot.jobs import reminder, weekly_report
-from tg_bot.jobs.jobs import drop_outdated_entries, mark_old_entries_to_delete
-
+from typer import Typer
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -21,60 +11,47 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-def nearest_weekday(day=0):
-    today = datetime.today()
-    diff = timedelta(days=(day - today.weekday()) % 7)
-    return today + diff
+typer = Typer()
 
 
-def main():
-    """
-    Configure app, create database if not exists, add scheduling jobs to bot and run it.
-    TODO: add cmd line args to use test token instead of prod for testing purposes
-    """
-    app = configure_app()
+@typer.command(name="tgbot")
+def tgbot(host: str, port: int, remote_url: str = '', test_config: bool = False):
+    from tg_bot.bot import start_bot
+    start_bot(host, port, remote_url, test_config)
+
+
+@typer.command()
+def webapp(host: str = "localhost", port: int = 8000, test_config: bool = False):
+    import os
+
+    import uvicorn
+
+    from webapp.core.settings import DB_FOLDER, init_test_settings
+    from webapp.fastapi_app import app
 
     dirs = os.listdir()
     if DB_FOLDER not in dirs:
         os.mkdir(DB_FOLDER)
 
-    conn = sqlite3.connect(DB_PATH)
+    if test_config:
+        init_test_settings()
 
-    if not db.check_base_table_exists(conn):
-        db.create_base_table(conn)
-
-    if not db.check_del_tmp_table_exists(conn):
-        db.create_del_tmp_table(conn)
-
-    for row in db.get_all_chat_alarms(conn):
-        try:
-            chat_id = row['chat_id']
-            alarm_time = datetime.strptime(row['alarm'], TIME_FORMAT).timetz()
-            job_name = f'{ALARM_JOB_PREFIX}{chat_id}'
-            app.job_queue.run_daily(reminder, alarm_time, name=job_name, chat_id=chat_id)
-
-        except (ValueError, KeyError) as ex:
-            logging.warning(f'Alarm job setting failed: {ex}')
-            raise
-
-    conn.close()
-
-    nearest_monday = nearest_weekday(0).replace(hour=23, minute=0, second=0, microsecond=0,
-                                                tzinfo=MSK_TIMEZONE_OFFSET)
-    app.job_queue.run_repeating(
-        weekly_report, timedelta(days=7), first=nearest_monday, name='weekly_report'
-    )
-    app.job_queue.run_repeating(
-        mark_old_entries_to_delete, timedelta(days=7), first=nearest_monday,
-        name='mark_old_entries_to_delete'
-    )
-    app.job_queue.run_monthly(
-        drop_outdated_entries, time(hour=23, minute=0, tzinfo=MSK_TIMEZONE_OFFSET), day=1,
-        name='drop_outdated_entries'
-    )
-
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    uvicorn.run(app, host=host, port=port)
 
 
-if __name__ == '__main__':
-    main()
+@typer.command()
+def workers(count: int = 4, test_config: bool = False):
+    from webapp.workers.reports.worker import worker
+
+    worker(count, test_config)
+
+
+@typer.command()
+def scheduler(test_config: bool = False):
+    from webapp.workers.scheduler.worker import worker
+
+    worker(test_config)
+
+
+if __name__ == "__main__":
+    typer()
--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,48 +1,104 @@
-from typing import Dict
-import aiosqlite
+import asyncio
 
-from telegram.ext import Application, ApplicationBuilder, PersistenceInput, PicklePersistence
+import uvicorn
+from fastapi import FastAPI, Request, Response, UploadFile
+from pydantic import BaseModel
+from telegram import Update
+from telegram.ext import (
+    Application,
+    ApplicationBuilder,
+    PersistenceInput,
+    PicklePersistence,
+)
 
-from tg_bot.constants import DB_PATH, HPJ_BOT_TOKEN, JINJA_TEMPLATE_PATH, JOURNAL_TEMPLATE, \
-    PERSISTENCE_PATH, OutputFileFormats
-from tg_bot.handlers import ALL_COMMAND_HANDLERS, ERROR_HANDLER
 from tg_bot.commands import DefaultMenuCommands
-from tg_bot.journal_view import HTMLGenerator, IFileGenerator
-
-
-def filegens() -> Dict[str, Dict[str, IFileGenerator]]:
-    return {
-        'file_generators': {
-            OutputFileFormats.HTML: HTMLGenerator(JINJA_TEMPLATE_PATH, JOURNAL_TEMPLATE),
-        },
-    }
+from tg_bot.constants import PERSISTENCE_PATH, bot_settings
+from tg_bot.handlers import ALL_COMMAND_HANDLERS, ERROR_HANDLER
+from tg_bot.handlers.webhooks import WebhookAlarmsUpdate, WebhookReportUpdate
 
 
 async def post_init(application: Application) -> None:
     """Post initialization handler. Updating bot data and setting commands for menu"""
-    conn = await aiosqlite.connect(DB_PATH)
-    db_data = {
-        'db_conn': conn,
-        'db_path': DB_PATH,
-    }
-    application.bot_data.update({
-        **db_data,
-        **filegens(),
-    })
     await application.bot.set_my_commands(DefaultMenuCommands().menu)
 
 
-def configure_app() -> Application:
+def configure_bot(test_config: bool = False) -> Application:
     """Configure and build app, add handlers"""
-    application = ApplicationBuilder() \
-        .token(HPJ_BOT_TOKEN) \
+    token = bot_settings.test_token if test_config else bot_settings.token
+    application = (
+        ApplicationBuilder()
+        .token(token)
         .persistence(
-            PicklePersistence(filepath=PERSISTENCE_PATH,
-                              store_data=PersistenceInput(bot_data=False))) \
-        .post_init(post_init) \
+            PicklePersistence(
+                filepath=PERSISTENCE_PATH, store_data=PersistenceInput(bot_data=False)
+            )
+        )
+        .post_init(post_init)
         .build()
+    )
 
     application.add_handlers(ALL_COMMAND_HANDLERS)
     application.add_error_handler(ERROR_HANDLER)
 
     return application
+
+
+class WebhookAlarmsRequest(BaseModel):
+    channel_ids: list[int]
+    time: str
+
+
+def configure_webapp(bot_app: Application) -> FastAPI:
+    app = FastAPI()
+
+    @app.post("/telegram")
+    async def telegram_updates(req: Request) -> Response:
+        await bot_app.update_queue.put(
+            Update.de_json(data=await req.json(), bot=bot_app.bot)
+        )
+        return Response()
+
+    @app.get("/healthcheck")
+    async def healthcheck() -> str:
+        return "bot is UP!"
+
+    @app.post("/webhooks/alarms")
+    async def alarms_webhook(body: WebhookAlarmsRequest):
+        for chat_id in body.channel_ids:
+            await bot_app.update_queue.put(
+                WebhookAlarmsUpdate(chat_id=chat_id, time=body.time)
+            )
+
+    @app.post("/webhooks/reports/{channel_id}")
+    async def reports_webhook(channel_id: int, file: UploadFile | None = None):
+        if file:
+            file_content, filename = await file.read(), file.filename
+        else:
+            file_content, filename = None, None
+
+        await bot_app.update_queue.put(
+            WebhookReportUpdate(
+                chat_id=channel_id,
+                report_file=file_content,
+                filename=filename,
+            )
+        )
+
+    return app
+
+
+async def run_bot(bot_app: Application, server: uvicorn.Server, webhook_url: str):
+    await bot_app.bot.set_webhook(url=webhook_url, allowed_updates=Update.ALL_TYPES)
+
+    async with bot_app:
+        await bot_app.start()
+        await server.serve()
+        await bot_app.stop()
+
+
+def start_bot(host: str, port: int, remote_url: str, test_config: bool = False):
+    bot_app = configure_bot(test_config)
+    webapp = configure_webapp(bot_app)
+    server = uvicorn.Server(uvicorn.Config(webapp, host=host, port=port))
+
+    asyncio.run(run_bot(bot_app, server, f"{host}:{port}/telegram"))
