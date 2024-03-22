c8b218510d9a2ce11ee7805139059bca3163e81d
NoOneWillSeeIt <rest338@gmail.com>
2024-03-22 15:05:50 +0700

    subscribe for webhooks route

--- webapp/api_v1/webhooks/subscribe.py
+++ webapp/api_v1/webhooks/subscribe.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
+from webapp.api_v1.common_dependencies import RedisDep
 from webapp.core.constants import Channel
+from webapp.core.redis import RedisKeys
 
-router = APIRouter(prefix='/webhooks', tags=['webhooks'])
-
-
-@router.post('/subscribe')
-async def subscribe(channel: Channel):
-    pass
+router = APIRouter(prefix="/webhooks", tags=["webhooks"])
 
 
-@router.post('/unsubscribe')
-async def unsubscribe(channel: Channel):
-    pass
+class SubscribeSchema(BaseModel):
+    channel: Channel
+    url: str
+
+
+@router.post("/subscribe")
+async def subscribe(body: SubscribeSchema, redis: RedisDep):
+    await redis.set(RedisKeys.webhooks_url(body.channel), body.url)
