fcbf8b3e64953b4fe0b92eac440d6d8398e7eee2
NoOneWillSeeIt <rest338@gmail.com>
2024-03-26 16:45:41 +0700

    removed constants

--- webapp/core/constants.py
+++
@@ -1,8 +0,0 @@
-from enum import StrEnum, auto
-
-
-class Channel(StrEnum):
-    telegram = auto()
-    # Bots from channels beneath aren't working, but shows idea behind this service
-    discord = auto()
-    whatsapp = auto()
