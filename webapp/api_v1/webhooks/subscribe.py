c1db7f1dbc2c6a2193b589aa9b718f64784bedeb
NoOneWillSeeIt <rest338@gmail.com>
2024-03-26 16:54:33 +0700

    unsubscribe webhooks functionality

--- webapp/api_v1/webhooks/subscribe.py
+++ webapp/api_v1/webhooks/subscribe.py
@@ -2,17 +2,29 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from webapp.api_v1.common_dependencies import RedisDep
-from webapp.core.constants import Channel
+from common.constants import Channel
 from webapp.core.redis import RedisKeys
 
 router = APIRouter(prefix="/webhooks", tags=["webhooks"])
 
 
-class SubscribeSchema(BaseModel):
+class WebhookSchema(BaseModel):
     channel: Channel
+
+
+class UnsubSchema(WebhookSchema):
+    pass
+
+
+class SubscribeSchema(WebhookSchema):
     url: str
 
 
 @router.post("/subscribe")
 async def subscribe(body: SubscribeSchema, redis: RedisDep):
     await redis.set(RedisKeys.webhooks_url(body.channel), body.url)
+
+
+@router.post("/unsubscribe")
+async def unsubscribe(body: UnsubSchema, redis: RedisDep):
+    await redis.delete(RedisKeys.webhooks_url(body.channel))
