27e3a56bce0956ca7b8ec755123c1e666792b25a
NoOneWillSeeIt <rest338@gmail.com>
2024-04-08 16:29:18 +0700

    html gens black formatter

--- webapp/workers/reports/journal_view/base_generator.py
+++ webapp/workers/reports/journal_view/base_generator.py
@@ -4,16 +4,15 @@ from abc import ABC, abstractmethod
 class IFileGenerator(ABC):
     """Base generator interface."""
 
-    _extension: str = ''
-    _name_prefix: str = 'hpj'
+    _extension: str = ""
+    _name_prefix: str = "hpj"
 
     @abstractmethod
-    def generate(self, replies: dict[str, str]) -> bytes:
-        ...
+    def generate(self, replies: dict[str, str]) -> bytes: ...
 
     @property
     def extension(self) -> str:
         return self._extension
 
     def gen_filename(self, name: str) -> str:
-        return f'{self._name_prefix}_{name}.{self.extension}'
+        return f"{self._name_prefix}_{name}.{self.extension}"
--- webapp/workers/reports/journal_view/html_generator.py
+++ webapp/workers/reports/journal_view/html_generator.py
@@ -3,34 +3,39 @@ from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
 
+from common.constants import ENTRY_DATE_FORMAT
+
 from .base_generator import IFileGenerator
 
 
 class HTMLGenerator(IFileGenerator):
     """HTML report generator."""
 
-    _extension = 'html'
+    _extension = "html"
 
-    def __init__(self, folder_path: str | Path, template_name: str,
-                 questions: dict[str, str]) -> None:
+    def __init__(
+        self, folder_path: str | Path, template_name: str, questions: dict[str, str]
+    ) -> None:
         """Init HTML generator.
 
         Args:
             folder_path (str | Path): Path to folder with templates.
             template_name (str): Template file name.
         """
-        self._env = Environment(loader=FileSystemLoader(folder_path, encoding='utf-8'))
+        self._env = Environment(loader=FileSystemLoader(folder_path, encoding="utf-8"))
         self._template = self._env.get_template(template_name)
         self._questions = questions
 
     def _prepare_render_params(self, replies: dict):
         """Prepares data for template."""
-        replies_keys = sorted(replies.keys(), key=lambda x: datetime.strptime(x, '%d.%m.%y'))
+        replies_keys = sorted(
+            replies.keys(), key=lambda x: datetime.strptime(x, ENTRY_DATE_FORMAT)
+        )
         params = {
-            'start_date': replies_keys[0],
-            'end_date': replies_keys[-1],
-            'replies_list': [self._questions] + [replies[key] for key in replies_keys],
-            'question_list': list(self._questions.keys()),
+            "start_date": replies_keys[0],
+            "end_date": replies_keys[-1],
+            "replies_list": [self._questions] + [replies[key] for key in replies_keys],
+            "question_list": list(self._questions.keys()),
         }
 
         return params
@@ -45,7 +50,5 @@ class HTMLGenerator(IFileGenerator):
             bytes: Generated file.
         """
 
-        str_file = self._template.render(
-            **self._prepare_render_params(replies)
-        )
-        return str_file.encode(encoding='utf-8')
+        str_file = self._template.render(**self._prepare_render_params(replies))
+        return str_file.encode(encoding="utf-8")
